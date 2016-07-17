23a2fb7b52904d6cf654d99a331774d4ada1fbda
TheerapatH <thetlpc@hotmail.com>
2016-07-18 00:02:08 +0700

    Add Build WebFront

---
+++ webfront/build/js/app/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('BSDShiftApp',['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise("/");
+  $stateProvider
+    .state('result', {
+      url : '/result',
+      templateUrl : '/js/app/result/template/result.tpl.html'
+    })
+    .state('assign', {
+      url : '/assign/:id',
+      controller : 'AssignController',
+      templateUrl : '/js/app/assign/template/assign.tpl.html'
+    })
+    .state('resultByDay', {
+      url : '/result/:day',
+      controller : 'ResultController',
+      templateUrl : '/js/app/result/template/result_day.tpl.html'
+    })
+});
+
+app.constant("AppConfig", {
+    "api_url" : "http://127.0.0.1:8080/"
+});
