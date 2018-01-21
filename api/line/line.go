df4f3ea129d6d5f40157dbe6573b639fb9d57f40
olipicus <olipicus@gmail.com>
2018-01-21 19:30:10 +0700

    Fix bot get Profile

--- api/line/line.go
+++ api/line/line.go
@@ -54,15 +54,13 @@ func (app *LineApp) CallbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("Got events %v", events)
 	for _, event := range events {
+		profile, err := app.bot.GetProfile(event.Source.UserID).Do()
 		if event.Type == linebot.EventTypeMessage {
+			if err != nil {
+				log.Fatal("Get Line Profile Error : " + err.Error())
+			}
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				profile, err := app.bot.GetProfile(event.Source.UserID).Do()
-
-				if err != nil {
-					log.Fatal("Get Line Profile Error : " + err.Error())
-				}
-
 				memberObj, err := app.memberService.GetMemberByLineID(ctx, profile.UserID)
 
 				if err == mgo.ErrNotFound {
